627562fc294d8e95204d62203d822100729c3799
Max Jacobson <max@hardscrabble.net>
2015-06-28 19:54:14 -0400

    Enable line length and documentation requirements

--- lib/todo_lint.rb
+++ lib/todo_lint.rb
@@ -1,5 +1,6 @@
 require "todo_lint/version"
 
+# here we establish the module which will namespace all of the code for this gem
 module TodoLint
   # Your code goes here...
 end
--- lib/todo_lint/version.rb
+++ lib/todo_lint/version.rb
@@ -1,3 +1,6 @@
+# this file's sole responsibility is noting the latest version for the gem,
+# it's in its own file so it can be required in the gemspec without requiring
+# everything else as well
 module TodoLint
   VERSION = "0.1.0"
 end
