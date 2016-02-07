c7f84852c78cb18851448bfe2953dcbf847e0856
Max Jacobson <max@hardscrabble.net>
2016-02-07 02:20:27 -0500

    Bump version to v0.4.0

--- lib/todo_lint/version.rb
+++ lib/todo_lint/version.rb
@@ -2,5 +2,5 @@
 # it's in its own file so it can be required in the gemspec without requiring
 # everything else as well
 module TodoLint
-  VERSION = "0.3.1".freeze
+  VERSION = "0.4.0".freeze
 end
