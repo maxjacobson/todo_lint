0d1061383ea205ef4c74edc64568e308ac1af990
Max Jacobson <max@hardscrabble.net>
2016-07-07 21:49:45 -0400

    some rubocop offenses

--- spec/todo_lint/due_date_spec.rb
+++ spec/todo_lint/due_date_spec.rb
@@ -13,14 +13,15 @@ module TodoLint
     describe "::from_annotation and #to_date" do
       it "parses dates from annotations" do
         due_date = DueDate.from_annotation("(2015-06-28)")
-        expect(due_date.to_date).to eq Date.new(2015, 06, 28)
+        expect(due_date.to_date).to eq Date.new(2015, 6, 28)
       end
 
       it "complains on invalid input" do
         expect do
           DueDate.from_annotation("omg")
         end.to raise_error(
-          ArgumentError, 'not a properly formatted date: "omg"')
+          ArgumentError, 'not a properly formatted date: "omg"'
+        )
       end
     end
 
